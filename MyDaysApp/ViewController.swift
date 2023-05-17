63832c3fd9e04f7d753bf6d901f995972feee7ff
ing911 <axmed.xamchiev@gmail.com>
2023-05-17 18:13:01 +0300

    Реализована логика приложения, при нажатии на кнопку в лейбле появляется информация про кол-во прожитых дней

--- MyDaysApp/ViewController.swift
+++ MyDaysApp/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak private var infoLabel: UILabel!
+    //@IBOutlet weak private var resultButton: UIButton! // св-во чтобы скруглить кнопку
+    private var numberOfDays = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //resultButton.layer.cornerRadius = 12 // скруглили кнопку по бокам
     }
 
-
+    @IBAction private func datePicker(_ sender: UIDatePicker) {
+        let range = sender.date..<Date.now
+        numberOfDays = range.formatted(.components(style: .wide, fields: [.day]))
+    }
+    
+    @IBAction private func resultButtonTapped() {
+        infoLabel.text = "Ты наслаждаешься жизнью уже \(numberOfDays)!"
+    }
 }
 
